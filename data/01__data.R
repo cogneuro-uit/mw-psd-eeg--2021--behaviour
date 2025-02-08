c82442d5e2d5cf83dc6584fda5fb9d1da7456b43
Steffen Aasen <github.plop432@slmail.me>
2025-02-08 14:44:17 +0100

    Add behavioural data reading script.

---
+++ data/01__data.R
@@ -0,0 +1,46 @@
+fnames=list.files("data/behaviour/", pattern="*.csv", full.names = T) 
+fnames=fnames[str_detect(fnames, "day")]
+
+data <- 
+  map_df(fnames, function(fname){
+  day = str_split(fname, "[/_]")[[1]][4]
+  dd <- read_csv(fname, comment = "#", col_types = cols(
+    subj = col_character(),
+    trial = col_double(),
+    time = col_double(),
+    stimulus = col_character(),
+    response = col_character()
+  )) %>%
+    mutate(day=day) -> dd
+  dd
+})
+
+
+# add block variable
+data <- 
+  data %>% 
+  group_by(subj,day) %>% 
+  do({
+    d=.
+    dd=d[d$stimulus=="probe1",]
+    dd$trial[-length(dd$trial)]
+    dd$trial1=c(0,dd$trial[-length(dd$trial)])
+    d$block=0
+    for(i in 1:dim(dd)[1]){
+      d$block[d$trial>dd$trial1[i] & d$trial<=dd$trial[i]]<-i
+    }
+    d
+})
+
+data <- 
+  data |> 
+  left_join(
+    demographics |> select(subj, group), 
+    by="subj"
+  ) |>
+  mutate(sleepdep=case_when(
+    group=="ESD" & day=="day1" ~ "SD",
+    group=="ESD" & day=="day2" ~ "control",
+    group=="LSD" & day=="day1" ~ "control",
+    group=="LSD" & day=="day2" ~ "SD",
+    T ~ "unddef")) 
