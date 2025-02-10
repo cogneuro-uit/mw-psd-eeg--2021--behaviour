8b4ecd07eee7860a7af34b5dc53ff386439d9f49
Steffen Aasen <github.plop432@slmail.me>
2025-02-10 10:04:29 +0100

    Update experiment feedback: - Bind research and participant feedback in one

--- data/06__experiment_feedback_data.R
+++ data/06__experiment_feedback_data.R
@@ -1,20 +1,20 @@
 # fix norwegian letters
 # Sys.setlocale("LC_ALL", "no_NO.UTF-8")     # ALL
 
-fnames <- 
-  list.files("data/baseline+diary", pattern = ".xlsx", full.names = T)
+fnames <- list.files("data/baseline+diary", pattern = ".xlsx", full.names = T)
 #' filter out open xlsx files:
 fnames <- fnames[!str_detect(fnames, "~")]
 
-# Read and get participants feedback        =======
-participant_feedback <- map(fnames, \(fname){
+message("Loading feedback comments...")
+
+feedback <- map(fnames, \(fname){
   print(fname)
   subj <- str_split(fname, "[/_]")[[1]][3]
   
-  d <- read_xlsx(fname, sheet = "Feedback-comments", col_names = F)
+  par <- read_xlsx(fname, sheet = "Feedback-comments", col_names = F)
   
   if(as.numeric(subj) < 5){
-    d |> 
+    par <- par |> 
       mutate(
         session = case_when( ...1 == "T1" ~ "S1", ...1 == "T2" ~ "S2" )
       ) |> 
@@ -40,14 +40,15 @@ participant_feedback <- map(fnames, \(fname){
           str_starts(q_que, "Når du rapporterte å ikke være") ~ "off_task_thought",
           str_starts(q_que, "Har du noen andre kommentarer?") ~ "comment_other",
         ),
-        q_ans = if_else(q_cat=="Answer", ...1, NA)
+        q_ans = if_else(q_cat=="Answer", ...1, NA),
+        par = "Par",
       ) |> 
       filter(!is.na(q_que) & q_cat=="Answer") |>
       select(-...1, -...2, -q_cat)  |> 
       mutate(subj = subj) |>
-      pivot_wider(names_from = c(session, q_que), values_from = q_ans) 
+      pivot_wider(names_from = c(session, par, q_que), values_from = q_ans) 
 } else {
-    d |>
+    par <- par |>
       mutate(
         session = case_when( ...1 == "T1" ~ "S1", ...1 == "T2" ~ "S2" )
       ) |> 
@@ -58,33 +59,33 @@ participant_feedback <- map(fnames, \(fname){
       mutate(q_que = rep(
         c("gloves", "AE_motivation", "MW_confidence", "task_strategy",
           "comment_task", "off_task_thought", "comment_other"), 2),
-        subj = subj
+        subj = subj,
+        par = "Par",
       ) |> 
       rename("Answer"=...2) |>
-      select("Answer", "session", "q_que", "subj") |> 
-      pivot_wider(names_from = c(session, q_que), values_from = "Answer")
+      select("Answer", "session", "q_que", "par", "subj") |> 
+      pivot_wider(names_from = c(session, par, q_que), values_from = "Answer")
   }
+  
+  res <- read_xlsx(fname, sheet = "DataSheet", range="B29:D37", col_names = FALSE) 
+  
+  res <- res |>
+    select(...1, ...3) |>
+    rename(que = ...1, ans = ...3) |>
+    filter(!is.na(que)) |>
+    mutate(
+      subj = subj, 
+      res = "exp",
+      seq = rep(c("S1","S2"), each=4),
+      que = case_when(
+        str_starts(que, "Protocol")  ~ "Protocol_compliance__side_effects", 
+        str_starts(que, "EEG") ~ "EEG_problems", 
+        str_starts(que, "Task-") ~ "Task_related", 
+        str_starts(que, "Else") ~ "Else", 
+      ),
+    ) |>
+    pivot_wider(names_from = c(seq, res, que), values_from = ans)
+  
+  par |> left_join(res, "subj")
 }) |> list_rbind()
 
-
-# Researchers comment to the experiment       ======
-research_comments <- 
-  map_df(fnames, \(x){
-    d <- 
-      tibble(
-        name = c("Protocol compliance/probs", "EEG prob", "task-related", "else", NA,
-                 "Protocol compliance/probs", "EEG prob", "task-related", "else")
-      ) |>
-      add_column(read_xlsx(x, sheet = "DataSheet", range="D29:D37", col_names="resp")) 
-    
-    if(is.null(d[["resp"]])){
-      d[["resp"]] <- ""
-    }
-    
-    d |>
-      filter(!is.na(name)) |>
-      mutate(seq = rep(c("T1","T2"), each=4)) |>
-      pivot_wider(names_from=seq, values_from=resp)
-})
-
-# save(research_comments, file = "data/research_comments.Rdata") 
