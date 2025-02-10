5a77b3ee828334f3dfb2c648a5784c4eb8a0cf8f
Steffen Aasen <github.plop432@slmail.me>
2025-02-10 09:37:27 +0100

    Update participant feedback code

--- data/06__experiment_feedback_data.R
+++ data/06__experiment_feedback_data.R
@@ -1,112 +1,70 @@
 # fix norwegian letters
-Sys.setlocale("LC_ALL", "no_NO.UTF-8")     # ALL
+# Sys.setlocale("LC_ALL", "no_NO.UTF-8")     # ALL
 
-list.files("data/baseline+diary", full.names = T) -> fnames
-
-fnames[!str_detect(fnames, "~")] -> fnames
+fnames <- 
+  list.files("data/baseline+diary", pattern = ".xlsx", full.names = T)
+#' filter out open xlsx files:
+fnames <- fnames[!str_detect(fnames, "~")]
 
 # Read and get participants feedback        =======
-
-question_names <- c("gloves", "AE_motivation", "MW_confidence", "task_strategy", 
-                    "comment_task", "off_task_thought", "comment_other",
-                    "quiz_fail","which_quet")
-
-participant_feedback <- data.frame()
-for(x in 1:length(fnames)){ 
-
-  read_xlsx(fnames[x],sheet = "Feedback-comments", col_names = F) -> d
+participant_feedback <- map(fnames, \(fname){
+  print(fname)
+  subj <- str_split(fname, "[/_]")[[1]][3]
   
-  par_df <- data.frame() # new for each par
+  d <- read_xlsx(fname, sheet = "Feedback-comments", col_names = F)
   
-  # 4 first are different 
-  if(x %in% 1:4){
-    for(y in 1:50){ # long
-      d$...1[y] -> y1
-      if(!is.na(y1) & str_detect(y1,"Q")){
-        if(str_detect(y1,"Quiz")){
-          d$...1[y] -> fb_quiz # quiz mistake
-          d$...2[y] -> fb_res  # *res*
-          d$...1[y+1] -> fb_quiz_fail # which question?
-          d$...2[y+1] -> fb_quiz_fail_r # *res*
-          c(fb_quiz,fb_quiz_fail) -> fb_quiz # collect
-          c(fb_res,fb_quiz_fail_r) -> fb_res # collect
-        } else {
-        d$...1[y] -> fb_quiz
-        d$...1[y+1] -> fb_res
-        }
-        rbind(par_df,tibble(fb_quiz,fb_res)) -> par_df
-      }
-    }
-    # add proper names
-    which(str_detect(par_df$fb_quiz, "ilket spørsmål")) -> end_1 # to quiz
-    end_1[2] -> end_2
-    end_1[1] -> end_1
-    par_df$fb_quiz[(end_1-(end_1-1)):end_1] <- question_names
-    par_df$fb_quiz[(end_2-(end_1-1)):end_2] <- question_names
-    
-    # Add participant code
-    strsplit(fnames[x], "\\/|\\_")[[1]][3] -> par_df$par_code 
-    # Add the different sessions
-    par_df$session <- rep(1:2,each=end_1) 
-    # Add group
-    read_xlsx(fnames[x], sheet = "DataSheet", 
-              range = "D6", col_names = F) |> 
-      mutate(...1 = ifelse(...1==0,"ESD","LSD")) |>
-      pull(...1) -> code_
-    
-    # Sleepdep first?
-    ifelse(code_=="ESD", 1,0) -> fc1
-    ifelse(fc1==1, 0,1) -> fc2
-    c(rep(fc1,end_1), rep(fc2,(end_2-end_1))) -> par_df$sleepdep
-    # Session
-    par_df$session <- rep(1:2,each=(end_1)) 
+  if(as.numeric(subj) < 5){
+    d |> 
+      mutate(
+        session = case_when( ...1 == "T1" ~ "S1", ...1 == "T2" ~ "S2" )
+      ) |> 
+      fill(session) |>
+      filter(
+        !(`...1` %in% c("T1","Feedback", "Open Feedback", "T2", "Quiz feil", "Hvilket spørsmål?", "Hvilke spørsmål?"))
+      ) |> 
+      mutate(
+        q_cat = case_when(
+          str_starts(`...1`, "Q")        ~ "Question",
+          lag( str_starts(`...1`, "Q") ) ~ "Answer",
+        ),
+        q_que = case_when( 
+          q_cat == "Question" ~ str_remove(...1, "Q\\d - "),
+          lag( q_cat == "Question" ) ~ lag( str_remove(...1, "Q\\d - ") )
+        ), 
+        q_que = case_when(
+          str_starts(q_que, "vennligst evaluer ") ~ "gloves",
+          str_starts(q_que, "Vennligst vurder hvor") ~ "AE_motivation",
+          str_starts(q_que, "Når du svarte på spørsmålet") ~ "MW_confidence",
+          str_starts(q_que, "Bruke du noen spesiell strategi") ~ "task_strategy",
+          str_starts(q_que, "Har du noen bemerkninger") ~ "comment_task",
+          str_starts(q_que, "Når du rapporterte å ikke være") ~ "off_task_thought",
+          str_starts(q_que, "Har du noen andre kommentarer?") ~ "comment_other",
+        ),
+        q_ans = if_else(q_cat=="Answer", ...1, NA)
+      ) |> 
+      filter(!is.na(q_que) & q_cat=="Answer") |>
+      select(-...1, -...2, -q_cat)  |> 
+      mutate(subj = subj) |>
+      pivot_wider(names_from = c(session, q_que), values_from = q_ans) 
+} else {
+    d |>
+      mutate(
+        session = case_when( ...1 == "T1" ~ "S1", ...1 == "T2" ~ "S2" )
+      ) |> 
+      fill(session) |>
+      filter(
+        (`...1` %in% c("Q1","Q2","Q3","Q4"))
+      ) |> 
+      mutate(q_que = rep(
+        c("gloves", "AE_motivation", "MW_confidence", "task_strategy",
+          "comment_task", "off_task_thought", "comment_other"), 2),
+        subj = subj
+      ) |> 
+      rename("Answer"=...2) |>
+      select("Answer", "session", "q_que", "subj") |> 
+      pivot_wider(names_from = c(session, q_que), values_from = "Answer")
   }
-  else {
-  # For every other case:  # I hope
-    for(y in 1:33){ # shorter
-      d$...1[y] -> y1
-      if(!is.na(y1) & str_detect(y1,"Q")){
-        if(str_detect(y1,"Quiz")){
-          d$...1[y] -> fb_quiz
-          d$...2[y] -> fb_res
-          d$...1[y+1] -> fb_quiz_fail
-          d$...2[y+1] -> fb_quiz_fail_r
-          c(fb_quiz,fb_quiz_fail) -> fb_quiz
-          c(fb_res,fb_quiz_fail_r) -> fb_res
-        } else {
-          d$...1[y] -> fb_quiz
-          d$...2[y] -> fb_res
-        }
-        rbind(par_df,tibble(fb_quiz,fb_res)) -> par_df
-      }
-    }
-    # Fix names (to column names)
-    which(str_detect(par_df$fb_quiz, "ilket spørsmål")) -> end_1 # we get 2
-    end_1[2] -> end_2 # last
-    end_1[1] -> end_1 # first
-    par_df$fb_quiz[(end_1-(end_1-1)):end_1] <- question_names
-    par_df$fb_quiz[(end_2-(end_1-1)):end_2] <- question_names
-    
-    # Add participant code
-    strsplit(fnames[x], "\\/|\\_")[[1]][3] -> par_df$par_code 
-    # add group
-    read_xlsx(fnames[1], sheet = "DataSheet", 
-              range = "D6", col_names = F) |> 
-      mutate(...1 = ifelse(...1==0,"ESD","LSD")) |>
-      pull(...1) -> code_
-    
-    # Sleepdep first?
-    ifelse(code_=="ESD", 1,0) -> fc1
-    ifelse(fc1==1, 0,1) -> fc2
-    c(rep(fc1,end_1), rep(fc2,(end_2-end_1))) -> par_df$sleepdep
-    # Session
-    par_df$session <- rep(1:2,each=(end_1))
-  }
-  # Bind
-  rbind(participant_feedback, par_df) -> participant_feedback
-}
-
-# save(participant_feedback, file="data/participant_feedback.rdata")
+}) |> list_rbind()
 
 
 # Researchers comment to the experiment       ======
