431d3fc947276b27fd3de0af83fa0d9c16c985b3
Steffen Aasen <github.plop432@slmail.me>
2025-02-11 13:44:00 +0100

    Update Bayesian model information

--- munge/20_bayesian_mood.R
+++ munge/20_bayesian_mood.R
@@ -1,42 +1,57 @@
-## Bayes models      ======
+
 if( project[["bayes"]][["run_models"]] ){
 
   mod_mood_full <- list()
-  
+
+  # MW  
   mod_mood_full$mw <- brm(
     mw ~ sleepdep * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood, 
     family=cumulative("probit"), cores = 6, backend = "cmdstanr",
     init = 0, chains = 6, iter = 6000)
-  # bayes_diag(mod_mood_full$mw)
+  # Diagnostics
+  bayes_diag(mod_mood_full$mw)
+  # Summary:
   bayes_tbl_sum(mod_mood_full$mw) |> gt()
   
+  # MB
   mod_mood_full$mb <- brm(
     mb ~ sleepdep * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood |> filter(mw > 2), family=cumulative("probit"), 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_mood_full$mb)
+  # Diagnostics
+  bayes_diag(mod_mood_full$mb)
+  # Summary:
   bayes_tbl_sum(mod_mood_full$mb)
-  
+
+  # SMW  
   mod_mood_full$smw <- brm(
     smw ~ sleepdep * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood |> filter(mw > 2), family=cumulative("probit"),
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_mood_full$smw)
+  # Diagnostics
+  bayes_diag(mod_mood_full$smw)
+  # Summary:
   bayes_tbl_sum(mod_mood_full$smw)
-  
+
+  # BV  
   mod_mood_full$bv <- brm(
     zlogbv ~ sleepdep * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood, 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_mood_full$bv)
+  # Diagnostics
+  bayes_diag(mod_mood_full$bv)
+  # Summary:
   bayes_tbl_sum(mod_mood_full$bv)
-  
+
+  # AE  
   mod_mood_full$ae <- brm(
     zlogapen ~ sleepdep * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood,
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_mood_full$ae)
+  # Diagnostics
+  bayes_diag(mod_mood_full$ae)
+  # Summary:
   bayes_tbl_sum(mod_mood_full$ae) |> gt()
   
   
@@ -47,14 +62,12 @@ if( project[["bayes"]][["run_models"]] ){
   mod_mood_full$ae  <- brms::add_criterion(mod_mood_full$ae,  c("bayes_R2", "loo")) 
   mod_mood_full$bv  <- brms::add_criterion(mod_mood_full$bv,  c("bayes_R2", "loo")) 
   
+  
   ### Save      =======
   if( project[["bayes"]][["save"]][["to_file"]] ){
-    save(
-      mod_mood_full,
-      
-      file = paste0(
-        "data/bayes_full_model_SR_exclude", 
-        project[["bayes"]][["save"]][["date_time"]],".Rdata")
+    save(mod_mood_full, file = paste0(
+      "data/bayes_full_model_SR_exclude", 
+      project[["bayes"]][["save"]][["date_time"]],".Rdata")
     )
   }
 } 
--- munge/21_bayesian_sleep_continuous.R
+++ munge/21_bayesian_sleep_continuous.R
@@ -3,41 +3,55 @@ if( project[["bayes"]][["run_models"]] ){
   
   mod_bay_sleep_cont <- list()
   
+  # MW
   mod_bay_sleep_cont$mw <- brm(
     mw ~ c.Duration.SD.pos * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep, 
-    family = cumulative("probit"), cores = 6, backend = "cmdstanr",
+    family = cumulative("probit"), backend = "cmdstanr",
     init = 0, chains = 6, iter = 6000)
-  # bayes_diag(mod_bay_sleep_cont$mw)
+  # Diagnostics
+  bayes_diag(mod_bay_sleep_cont$mw)
+  # Summary:
   bayes_tbl_sum(mod_bay_sleep_cont$mw) |> gt()
-  bayes_tbl_sum(test) |> gt()
   
+  # MB
   mod_bay_sleep_cont$mb <- brm(
     mb ~ c.Duration.SD.pos * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep |> filter(mw > 2), family = cumulative("probit"), 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_bay_sleep_cont$mb)
+  # Diagnostics
+  bayes_diag(mod_bay_sleep_cont$mb)
+  # Summary:
   bayes_tbl_sum(mod_bay_sleep_cont$mb)
   
+  # SMW
   mod_bay_sleep_cont$smw <- brm(
     smw ~ c.Duration.SD.pos * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep |> filter(mw > 2), family = cumulative("probit"),
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_bay_sleep_cont$smw)
+  # Diagnostics
+  bayes_diag(mod_bay_sleep_cont$smw)
+  # Summary:
   bayes_tbl_sum(mod_bay_sleep_cont$smw)
   
+  # BV
   mod_bay_sleep_cont$bv <- brm(
     zlogbv ~ c.Duration.SD.pos * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep, 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_bay_sleep_cont$bv)
+  # Diagnostics
+  bayes_diag(mod_bay_sleep_cont$bv)
+  # Summary:
   bayes_tbl_sum(mod_bay_sleep_cont$bv)
   
+  # AE
   mod_bay_sleep_cont$ae <- brm(
     zlogapen ~ c.Duration.SD.pos * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep,
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # bayes_diag(mod_bay_sleep_cont$ae)
+  # Diagnostics
+  bayes_diag(mod_bay_sleep_cont$ae)
+  # Summary:
   bayes_tbl_sum(mod_bay_sleep_cont$ae) |> gt()
   
   
@@ -48,12 +62,12 @@ if( project[["bayes"]][["run_models"]] ){
   mod_bay_sleep_cont$ae  <- brms::add_criterion(mod_bay_sleep_cont$ae,  c("bayes_R2", "loo")) 
   mod_bay_sleep_cont$bv  <- brms::add_criterion(mod_bay_sleep_cont$bv,  c("bayes_R2", "loo")) 
   
+  
   ### Save      =======
   if( project[["bayes"]][["save"]][["to_file"]] ){
-    save(
-      mod_bay_sleep_cont, file = paste0(
-        "data/bayes_model_cont_SD", 
-        project[["bayes"]][["save"]][["date_time"]] , ".rdata")
+    save(mod_bay_sleep_cont, file = paste0(
+      "data/bayes_model_cont_SD", 
+      project[["bayes"]][["save"]][["date_time"]] , ".rdata")
     )
   }
 } 
