cdec2e093deac6ef2d55f25db14d208504696442
Steffen Aasen <github.plop432@slmail.me>
2025-02-11 14:10:00 +0100

    Update continous SD Bayes model: - Add parameter for diagnostics - Update model data (to use positive coded SD varaible)

--- munge/21_bayesian_sleep_continuous.R
+++ munge/21_bayesian_sleep_continuous.R
@@ -9,51 +9,60 @@ if( project[["bayes"]][["run_models"]] ){
     data = data.probe.mood.sleep, 
     family = cumulative("probit"), backend = "cmdstanr",
     init = 0, chains = 6, iter = 6000)
-  # Diagnostics
-  bayes_diag(mod_bay_sleep_cont$mw)
-  # Summary:
-  bayes_tbl_sum(mod_bay_sleep_cont$mw) |> gt()
+  if( project[["bayes"]][["set"]][["diagnostic_feedback"]] ){
+    # Diagnostics
+    bayes_diag(mod_bay_sleep_cont$mw)
+    # Summary:
+    bayes_tbl_sum(mod_bay_sleep_cont$mw) |> gt()
+  }
   
   # MB
   mod_bay_sleep_cont$mb <- brm(
     mb ~ c.Duration.SD.pos * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep |> filter(mw > 2), family = cumulative("probit"), 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # Diagnostics
-  bayes_diag(mod_bay_sleep_cont$mb)
-  # Summary:
-  bayes_tbl_sum(mod_bay_sleep_cont$mb)
+  if( project[["bayes"]][["set"]][["diagnostic_feedback"]] ){
+    # Diagnostics
+    bayes_diag(mod_bay_sleep_cont$mb)
+    # Summary:
+    bayes_tbl_sum(mod_bay_sleep_cont$mb)
+  }
   
   # SMW
   mod_bay_sleep_cont$smw <- brm(
     smw ~ c.Duration.SD.pos * (zlogapen + zlogbv + probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep |> filter(mw > 2), family = cumulative("probit"),
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # Diagnostics
-  bayes_diag(mod_bay_sleep_cont$smw)
-  # Summary:
-  bayes_tbl_sum(mod_bay_sleep_cont$smw)
+  if( project[["bayes"]][["set"]][["diagnostic_feedback"]] ){
+    # Diagnostics
+    bayes_diag(mod_bay_sleep_cont$smw)
+    # Summary:
+    bayes_tbl_sum(mod_bay_sleep_cont$smw)
+  }
   
   # BV
   mod_bay_sleep_cont$bv <- brm(
     zlogbv ~ c.Duration.SD.pos * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep, 
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # Diagnostics
-  bayes_diag(mod_bay_sleep_cont$bv)
-  # Summary:
-  bayes_tbl_sum(mod_bay_sleep_cont$bv)
+  if( project[["bayes"]][["set"]][["diagnostic_feedback"]] ){
+    # Diagnostics
+    bayes_diag(mod_bay_sleep_cont$bv)
+    # Summary:
+    bayes_tbl_sum(mod_bay_sleep_cont$bv)
+  }
   
   # AE
   mod_bay_sleep_cont$ae <- brm(
     zlogapen ~ c.Duration.SD.pos * (probenum + pre_pos + pre_neg) + (1|subj), 
     data = data.probe.mood.sleep,
     init = 0, chains = 6, iter = 6000, cores = 6, backend = "cmdstanr")
-  # Diagnostics
-  bayes_diag(mod_bay_sleep_cont$ae)
-  # Summary:
-  bayes_tbl_sum(mod_bay_sleep_cont$ae) |> gt()
-  
+  if( project[["bayes"]][["set"]][["diagnostic_feedback"]] ){
+    # Diagnostics
+    bayes_diag(mod_bay_sleep_cont$ae)
+    # Summary:
+    bayes_tbl_sum(mod_bay_sleep_cont$ae) |> gt()
+  }
   
   # Criteria 
   mod_bay_sleep_cont$mw  <- brms::add_criterion(mod_bay_sleep_cont$mw,  c("bayes_R2", "loo")) 
