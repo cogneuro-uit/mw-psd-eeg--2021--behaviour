fa1bd27299b6571641017c39431d4326a988c3d4
Steffen Aasen <github.plop432@slmail.me>
2025-02-11 13:31:52 +0100

    Rm old muning files

--- munge/sleep_time_transformations.R
+++
@@ -1,6 +0,0 @@
-# bind self-report (SR) and actigraphy (AG) 
-sleeptimes <- 
-  sleep_diary |>
-  left_join(actigraphy, by=c("subj","date"="date_end_ag"))
-
-# Bind adjusted sleep times: 
--- munge/sleeptimes.R
+++
@@ -1,3 +0,0 @@
-sleeptimes <- 
-  sleep_diary |>
-  left_join(actigraphy, by=c("subj","date"="date_end_ag"))
