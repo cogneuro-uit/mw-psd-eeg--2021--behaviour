00d40f15dbf006cfd51e81d7616f7aea7ff54cd4
Steffen Aasen <github.plop432@slmail.me>
2025-02-12 08:53:53 +0100

    Update load_project to work without other libs

--- lib/load_project.R
+++ lib/load_project.R
@@ -2,14 +2,14 @@ load_project <- function(relative_path){
   #' Function to load libraries, data, and munging for .qmds.
   
   # Get higher-order libraries
-  load_libraries <- map(
+  load_libraries <- lapply(
     read.dcf(paste0(relative_path, "config/global.dcf"))[,"libraries"], 
-    ~ str_split(.x, ", ") )[[1]][[1]]
+    \(x){ strsplit(x, ", ") } )[[1]][[1]]
   
   # Load higher-order libraries
-  walk(load_libraries, \(x){
-    cat("Loading library:", x, "\n") 
-    library(x, character.only = T) 
+  lapply(load_libraries, \(library){
+    cat("Loading library:", library, "\n")
+    library(library, character.only = T)
   })
   
   # Get local libraries, data, and munge paths...
@@ -23,8 +23,13 @@ load_project <- function(relative_path){
                    get_path("data",  "\\.rdata"),
                    get_path("munge", "\\.r") )
   
+  # Load rccp
+  lapply(list.files(
+    paste0(relative_path,"lib"), pattern = "*\\.cpp", full.names = TRUE),
+    \(x) Rcpp::sourceCpp(x) )
+  
   # Load local data/functions/pre-processing
-  walk(load_path, \(path){
+  lapply(load_path, \(path){
     message("Loading ", path)
     
     if( str_ends(path, regex("\\.rdata", ignore_case=T)) ){
