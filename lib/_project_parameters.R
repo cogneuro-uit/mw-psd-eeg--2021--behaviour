0eb0d181f788f42567d6ba705d340627983b6446
Steffen Aasen <github.plop432@slmail.me>
2025-03-03 15:34:18 +0100

    Update project parameters

--- lib/_project_parameters.R
+++ lib/_project_parameters.R
@@ -9,19 +9,20 @@ options(
 
   # Figure related settings
   project_save_figs_to_file = TRUE,
-  project_save_figs_location = "/outputs/figs/",
+  project_save_figs_location = "outputs/figs/",
   project_save_figs_formats = c(".svg", ".jpeg"),
-  project_save_figs_with_date_time = TRUE,
+  project_save_figs_with_date_time = FALSE,
 
   # Table related settings
-  project_save_tbls_location = "/outputs/tbls/",
+  project_save_tbls_location = "outputs/tbls/",
   project_save_tbls_to_file = TRUE,
   project_save_tbls_formats = c(".html", ".docx"),
-  project_save_tbls_with_date_time = TRUE,
+  project_save_tbls_with_date_time = FALSE,
   
   # Aesthetics: 
   project_custom_ggplot = TRUE,
-  project_custom_ggplot_colour = c("#F8766D", "#619CFF"),
+  project_custom_ggplot_colours = c("#F8766D", "#619CFF", "#f8ea6d"),
+  project_custom_ggplot_colours2 = c("#619CFF","#0046bc","#F8766D","#b20b00"),
 
   # Bayesian options:
   project_bayes_run_models = FALSE,
@@ -50,10 +51,10 @@ Sys.setenv("_R_CHECK_LENGTH_1_CONDITION_"=1)
 #'  custom colours for ggplot (if enabled)
 if( getOption("project_custom_ggplot") ){
   options(
-    ggplot2.continous.colour = getOption("project_custom_ggplot_colour"),
-    ggplot2.continous.fill   = getOption("project_custom_ggplot_colour"), 
-    ggplot2.discrete.colour  = getOption("project_custom_ggplot_colour"), 
-    ggplot2.discrete.fill    = getOption("project_custom_ggplot_colour")
+    ggplot2.continous.colour = getOption("project_custom_ggplot_colours"),
+    ggplot2.continous.fill   = getOption("project_custom_ggplot_colours"), 
+    ggplot2.discrete.colour  = getOption("project_custom_ggplot_colours"), 
+    ggplot2.discrete.fill    = getOption("project_custom_ggplot_colours")
   )
   theme_set( theme_bw() )
 }
