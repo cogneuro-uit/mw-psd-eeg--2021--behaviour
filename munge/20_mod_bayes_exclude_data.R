f9fed07a3e99fdf3134b4b62b90784ab5088b27d
Steffen Aasen <github.plop432@slmail.me>
2025-03-03 15:35:52 +0100

    Update model name + saved model

--- munge/20_mod_bayes_exclude_data.R
+++ munge/20_mod_bayes_exclude_data.R
@@ -6,93 +6,93 @@ if( getOption("project_bayes_run_models") ){
     data.probe.mood.sleep |>
     filter(Adjusted_Duration.diff < -1.5) 
   
-  mod_mood_full <- list()
+  mod_bay_split_sleep <- list()
 
   # MW  
-  mod_mood_full$mw <- brm(
+  mod_bay_split_sleep$mw <- brm(
     mw ~ sleepdep * (zlogapen + zlogbv + probenum_prop + pre_pos + pre_neg) + (1|subj), 
     data = selected_data.probe.mood.sleep, 
     family = cumulative("probit"), init = 0, chains = 6, iter = 6000)
   
   if( getOption("project_bayes_diagnostics") ){ 
-    brms::pp_check(mod_mood_full$mw, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$mw) # chain stability
-    bayes_diag(mod_mood_full$mw) # general model fit and coefficients
-    bayes_coef_plot(mod_mood_full$mw) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$mw) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$mw, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$mw) # chain stability
+    bayes_diag(mod_bay_split_sleep$mw) # general model fit and coefficients
+    bayes_coef_plot(mod_bay_split_sleep$mw) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$mw) # coefficients (table)
   }
   
   # MB
-  mod_mood_full$mb <- brm(
+  mod_bay_split_sleep$mb <- brm(
     mb ~ sleepdep * (zlogapen + zlogbv + probenum_prop + pre_pos + pre_neg) + (1|subj), 
     data = selected_data.probe.mood.sleep |> filter(mw > 2), 
     family = cumulative("probit"), init = 0, chains = 6, iter = 6000)
   
   if( getOption("project_bayes_diagnostics") ){ 
-    brms::pp_check(mod_mood_full$mb, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$mb) # chain stability
-    bayes_diag(mod_mood_full$mb) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$mb) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$mb, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$mb) # chain stability
+    bayes_diag(mod_bay_split_sleep$mb) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$mb) # coefficients (table)
   }
   
   # SMW  
-  mod_mood_full$smw <- brm(
+  mod_bay_split_sleep$smw <- brm(
     smw ~ sleepdep * (zlogapen + zlogbv + probenum_prop + pre_pos + pre_neg) + (1|subj), 
     data = selected_data.probe.mood.sleep |> filter(mw > 2), 
     family = cumulative("probit"), init = 0, chains = 6, iter = 6000)
   
   if( getOption("project_bayes_diagnostics") ){ 
-    brms::pp_check(mod_mood_full$smw, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$smw) # chain stability
-    bayes_diag(mod_mood_full$smw) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$smw) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$smw, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$smw) # chain stability
+    bayes_diag(mod_bay_split_sleep$smw) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$smw) # coefficients (table)
   }
   
   # BV  
-  mod_mood_full$bv <- brm(
+  mod_bay_split_sleep$bv <- brm(
     zlogbv ~ sleepdep * (probenum_prop + pre_pos + pre_neg) + (1|subj), 
     data = selected_data.probe.mood.sleep, 
     init = 0, chains = 6, iter = 6000)
   
   if( getOption("project_bayes_diagnostics") ){ 
-    brms::pp_check(mod_mood_full$bv, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$bv) # chain stability
-    bayes_diag(mod_mood_full$bv) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$bv) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$bv, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$bv) # chain stability
+    bayes_diag(mod_bay_split_sleep$bv) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$bv) # coefficients (table)
   }
   if( getOption("project_bayes_diagnostics") ){ 
-    brms::pp_check(mod_mood_full$bv, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$bv) # chain stability
-    bayes_diag(mod_mood_full$bv) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$bv) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$bv, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$bv) # chain stability
+    bayes_diag(mod_bay_split_sleep$bv) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$bv) # coefficients (table)
   }
   
   # AE  
-  mod_mood_full$ae <- brm(
+  mod_bay_split_sleep$ae <- brm(
     zlogapen ~ sleepdep * (probenum_prop + pre_pos + pre_neg) + (1|subj), 
     data = selected_data.probe.mood.sleep,
     init = 0, chains = 6, iter = 6000)
   
   if( getOption("project_bayes_diagnostics") ){
-    brms::pp_check(mod_mood_full$ae, ndraws=50) # Predictive check
-    bayes_chain_stab(mod_mood_full$ae) # chain stability
-    bayes_diag(mod_mood_full$ae) # general model fit and coefficients
-    # bayes_tbl_sum(mod_mood_full$ae) # coefficients (table)
+    brms::pp_check(mod_bay_split_sleep$ae, ndraws=50) # Predictive check
+    bayes_chain_stab(mod_bay_split_sleep$ae) # chain stability
+    bayes_diag(mod_bay_split_sleep$ae) # general model fit and coefficients
+    # bayes_tbl_sum(mod_bay_split_sleep$ae) # coefficients (table)
   }
   
   # Criteria 
-  mod_mood_full$mw  <- brms::add_criterion(mod_mood_full$mw,  c("bayes_R2", "loo")) 
-  mod_mood_full$mb  <- brms::add_criterion(mod_mood_full$mb,  c("bayes_R2", "loo")) 
-  mod_mood_full$smw <- brms::add_criterion(mod_mood_full$smw, c("bayes_R2", "loo")) 
-  mod_mood_full$ae  <- brms::add_criterion(mod_mood_full$ae,  c("bayes_R2", "loo")) 
-  mod_mood_full$bv  <- brms::add_criterion(mod_mood_full$bv,  c("bayes_R2", "loo")) 
+  mod_bay_split_sleep$mw  <- brms::add_criterion(mod_bay_split_sleep$mw,  c("bayes_R2", "loo")) 
+  mod_bay_split_sleep$mb  <- brms::add_criterion(mod_bay_split_sleep$mb,  c("bayes_R2", "loo")) 
+  mod_bay_split_sleep$smw <- brms::add_criterion(mod_bay_split_sleep$smw, c("bayes_R2", "loo")) 
+  mod_bay_split_sleep$ae  <- brms::add_criterion(mod_bay_split_sleep$ae,  c("bayes_R2", "loo")) 
+  mod_bay_split_sleep$bv  <- brms::add_criterion(mod_bay_split_sleep$bv,  c("bayes_R2", "loo")) 
   
   
   ### Save      =======
   if( getOption("project_bayes_save_to_file")  ){
     time <- ""
     if( getOption("project_bayes_save_with_date_time") ) time <- getOption("project_date_timne")
-    save(mod_mood_full, file = paste0(
+    save(mod_bay_split_sleep, file = paste0(
       "data/mod_bayes_exclude-1.5h", time,".Rdata")
     )
   }
