6dd748aba52c3210190d892273ac8a5108a7f813
Steffen Aasen <github.plop432@slmail.me>
2025-04-04 15:33:09 +0200

    Update munge structure (4): transformations + PSD over report types

---
+++ munge/4_figures/00_trans.R
@@ -0,0 +1,10 @@
+name_colour_interactions <- c(
+  "NS"        = gen_col("b"),
+  "PSD -1 SD" = gen_col(".r"),
+  "PSD Mean"  = gen_col("r"), 
+  "PSD +1 SD" = gen_col("_R"))
+name_line_interactions <- c(
+  "NS"        = "dotted", 
+  "PSD -1 SD" = "dashed",
+  "PSD Mean"  = "solid", 
+  "PSD +1 SD" = "longdash")
