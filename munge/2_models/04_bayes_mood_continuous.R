df588f1ed47dff29848bfa6c4548006082a20ef1
Steffen Aasen <github.plop432@slmail.me>
2025-05-07 11:59:44 +0200

    Update munge-mood bayes models

--- munge/2_models/04_bayes_mood_continuous.R
+++ munge/2_models/04_bayes_mood_continuous.R
@@ -3,43 +3,49 @@ if( getOption("project_bayes_run_models") ){
   mod.mood.cont <- list()
   
   # DV: Positive mood        =======
-  
-  # Model
-  mod.mood.cont[["bayes"]][["pos"]] <- brm(
-    PANASsum_0 ~ c.Adjusted_Duration.diff.pos * prepost + (c.Adjusted_Duration.diff.pos * prepost | subj),
-    data = test_panas |> filter(valence=="pos"),
-    init = 0, iter = 6000, chains = 6) |>
+  ## PSD on pre-task mood       ======
+  mod.mood.cont[["pos"]][["psd"]] <- brm(
+    PANASsum_0_pre ~ c.Adjusted_Duration.diff.pos + (1 | subj)
+    , data = test_panas_diff |> filter(valence=="pos")
+    , init = 0, iter = 6000, chains = 6) |>
     add_criterion(c("bayes_R2", "loo", "loo_R2"))
+  brms::pp_check(  mod.mood.cont$pos$psd, ndraws=50)
+  bayes_chain_stab(mod.mood.cont$pos$psd) 
+  bayes_diag(      mod.mood.cont$pos$psd) 
   
-  # Diagnostics
-  if( getOption("project_bayes_diagnostics") ){ 
-    #' 1. Predictive check, to check the fit of the outcome 
-    #' 2. Chain stability
-    #' 3. Model fit + coefficients 
-    brms::pp_check(  mod.mood.cont[["bayes"]][["pos"]], ndraws=50)
-    bayes_chain_stab(mod.mood.cont[["bayes"]][["pos"]]) 
-    bayes_diag(      mod.mood.cont[["bayes"]][["pos"]]) 
-    # bayes_tbl_sum(mod.mood.cont[["pos"]][["bayes"]][["cont"]]) # coefficients (table)
-  }
-  
-  # DV: Negative mood         =======
-  # Model
-  mod.mood.cont[["bayes"]][["neg"]] <- brm(
-    PANASsum_0 ~ c.Adjusted_Duration.diff.pos * prepost + (c.Adjusted_Duration.diff.pos * prepost | subj),
-    data = test_panas |> filter(valence=="neg"),
-    init = 0, iter = 6000, chains = 6) |>
+  ## PSD x MW on mood change      =====
+  mod.mood.cont[["pos"]][["psdXmw"]] <- brm(
+    pos_diff ~ c.Adjusted_Duration.diff.pos * mw + (1 | subj)
+    , data = mood_diff_test
+    , init = 0, iter = 6000, chains = 6) |>
     add_criterion(c("bayes_R2", "loo", "loo_R2"))
+  brms::pp_check(  mod.mood.cont$pos$psdXmw, ndraws=50)
+  bayes_chain_stab(mod.mood.cont$pos$psdXmw) 
+  bayes_diag(      mod.mood.cont$pos$psdXmw) 
   
-  # Diagnostics
-  if( getOption("project_bayes_diagnostics") ){ 
-    #' 1. Predictive check, to check the fit of the outcome 
-    #' 2. Chain stability
-    #' 3. Model fit + coefficients 
-    brms::pp_check(  mod.mood.cont[["bayes"]][["neg"]], ndraws=50)
-    bayes_chain_stab(mod.mood.cont[["bayes"]][["neg"]])
-    bayes_diag(      mod.mood.cont[["bayes"]][["neg"]])
-    # bayes_tbl_sum(mod.mood.cont[["neg"]][["bayes"]][["cont"]])
-  }
-
+  
+  # DV: Negative mood        =======
+  ## PSD on mood      =====
+  mod.mood.cont[["neg"]][["psd"]] <- brm(
+    PANASsum_0_pre ~ c.Adjusted_Duration.diff.pos + (1 | subj)
+    , data = test_panas_diff |> filter(valence=="neg")
+    , init = 0, iter = 6000, chains = 6) |>
+    add_criterion(c("bayes_R2", "loo", "loo_R2"))
+  brms::pp_check(  mod.mood.cont$neg$psd, ndraws=50)
+  bayes_chain_stab(mod.mood.cont$neg$psd) 
+  bayes_diag(      mod.mood.cont$neg$psd) 
+  
+  ## PSD x MW on mood change      ======
+  mod.mood.cont[["neg"]][["psdXmw"]] <- brm(
+    neg_diff ~ c.Adjusted_Duration.diff.pos * mw + (1 | subj)
+    , data = mood_diff_test
+    , init = 0, iter = 6000, chains = 6) |>
+    add_criterion(c("bayes_R2", "loo", "loo_R2"))
+  brms::pp_check(  mod.mood.cont$neg$psdXmw, ndraws=50)
+  bayes_chain_stab(mod.mood.cont$neg$psdXmw) 
+  bayes_diag(      mod.mood.cont$neg$psdXmw) 
+  
+  
+  # Save  =====
   conditional_save(mod.mood.cont, "PSD-on-Mood_Continuous")  
 }
