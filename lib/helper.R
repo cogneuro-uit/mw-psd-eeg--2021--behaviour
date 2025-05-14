857e6a0ca15c6f2cf806edbb68e3529ec33c506b
Steffen Aasen <github.plop432@slmail.me>
2025-05-14 16:41:39 +0200

    Update bayesian_reporting & helper scripts

--- lib/helper.R
+++ lib/helper.R
@@ -2,11 +2,16 @@
 
 clock_24 <- function(data){
   map(data, \(d){
-    d2 <- str_split(d, "\\.")[[1]][1]
-    if( str_length(d2[1]) == 1) d2[1] <- paste0("0", d2[1])
-    d3 <- str_split( round((as.numeric(d) - as.numeric(d2)) * .6, digits = 2), "\\.")[[1]][2]
-    if(is.na(d3)) d3 <- "00"
-    str_glue("{d2}:{d3}")
+    if(is.na(d)){
+      NA
+    } else {
+      d2 <- str_split(d, "\\.")[[1]][1]
+      if( str_length(d2[1]) == 1) d2[1] <- paste0("0", d2[1])
+      d3 <- str_split( round((as.numeric(d) - as.numeric(d2)) * .6, digits = 2), "\\.")[[1]][2]
+      if(is.na(d3)) d3 <- "00" # if it does not exist, assume it is 00
+      if(str_length(d3)<2) d3 <- paste0(d3,"0") # if length is not 2, assume we are missing 0
+      str_glue("{d2}:{d3}")
+    }
   }) |> list_c()
 }
 
