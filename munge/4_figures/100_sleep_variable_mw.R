51c8bb4f56fc3b65e6a53a3f56e030f65da23845
Steffen Aasen <github.plop432@slmail.me>
2025-07-03 14:57:24 +0200

    Update fig - improve PSD x MW relationship figure

--- munge/4_figures/100_sleep_variable_mw.R
+++ munge/4_figures/100_sleep_variable_mw.R
@@ -1,5 +1,4 @@
-
-figs[["sleep_variables_mw"]] <- 
+figs[["sleep_variables_mw"]] <-
   data.probe.mood.sleep |>
   mutate(sleepdep = fct_relevel(sleepdep, "SD")) |>
   pivot_longer(c(c.Self.report_Duration.diff.pos, c.Actigraphy_Duration.diff.pos, c.Adjusted_Duration.diff.pos)) |>
@@ -8,25 +7,44 @@ figs[["sleep_variables_mw"]] <-
     sleep = unique(value),
     mw = mean(as.numeric(mw)),
   ) |>
-  mutate(name = case_when(
-    str_detect(name, "Self.report") ~ "Self report"
-    , str_detect(name, "Actigraphy") ~ "Actigraphy"
-    , str_detect(name, "Adjusted") ~ "Adjusted") |> fct_relevel(c("Self report", "Actigraphy"))
+  mutate(
+    name = case_when(
+      str_detect(name, "Self.report") ~ "Self report"
+      , str_detect(name, "Actigraphy") ~ "Actigraphy"
+      , str_detect(name, "Adjusted") ~ "Adjusted"
+    ) |> fct_relevel(c("Self report", "Actigraphy"))
     , Condition = if_else(sleepdep=="SD", "PSD", "NS") |> fct_relevel("PSD")
     , modalityXcondition = case_when(
       Condition == "NS"    ~ "NS"
       , Condition == "PSD" ~ paste0(name," PSD")
     ) |> factor(levels = c("NS", "Self report PSD", "Actigraphy PSD", "Adjusted PSD"))
     , modalityXsubj       = paste0(name, "_", subj)
-  ) |>
-  ggplot(aes(sleep, mw, col = modalityXcondition, group = modalityXsubj, alpha = Condition)) +
-  geom_point(alpha = .5) +
-  geom_line(aes(col = NULL), alpha = .05) +
-  scale_alpha_manual(values = c("PSD"=.2, "NS"=0.01)) +
-  geom_smooth(aes(group = name, col = name), method = "lm", alpha = .08) +
-  ggrepel::geom_text_repel(aes(label = if_else(sleep == 0, NA, subj)), alpha = .6) +
-  scale_colour_manual(values = gen_col("/G-r-b-grbg")) +
-  labs(x = "Average sleep deprivation", y = "Mind wandering", col = "Condition")
+    # highlight certain partic
+    , highlight_subj = case_when(
+      subj %in% c("003", "012", "005", "017", "023") ~ T
+      , T ~ F )
+    , alpha_dots = case_when(
+      highlight_subj == T & Condition == "PSD" ~ "Highlight_dot"
+      , T ~ Condition )
+    , alpha_lines = case_when(
+      highlight_subj == T ~ "Highlight_line"
+      , T ~ "weak_line"
+    )
+  ) |> 
+  ggplot(aes(sleep, mw, col = modalityXcondition, group = modalityXsubj)) +
+  # aesthetics
+  scale_colour_manual(values = gen_col("/G-r-b-grbg")) + # + the general (smooth) <<lines
+  scale_alpha_manual(values = c("NS" = 0.0, "PSD" = 0.25, "Highlight_dot" = .9,
+                                "Highlight_line" = .4, "weak_line" = .035)) +
+  # connecting lines
+  geom_line(aes(col = NULL, alpha = alpha_lines)) +
+  # Dots
+  geom_point(aes(alpha = alpha_dots)) +
+  geom_text_repel(aes(label = if_else(sleep == 0, NA, subj), alpha = alpha_dots)) +
+  # grand lines 
+  geom_line(aes(group = name, col = name), stat = "smooth", method = "lm", alpha = .7, linewidth = .7) +
+  labs(x = "Average sleep deprivation", y = "Mind wandering", col = "Condition") +
+  guides(alpha = "none")
 
 conditional_save(
   figs[["sleep_variables_mw"]]
