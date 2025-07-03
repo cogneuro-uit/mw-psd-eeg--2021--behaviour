22c20f6d37ca9bc416b995a5889a0ce255cd66f1
Steffen Aasen <github.plop432@slmail.me>
2025-07-03 14:59:53 +0200

    Update tbl cont probes + add cont behav

---
+++ munge/5_tables/011_cont_bayes_behaviour.R
@@ -0,0 +1,56 @@
+
+tbls[["cont_behav_models"]] <- 
+  # Continuous
+  bayes_tbl_sum(mod.cont$bv, apa_table = T, add_loo_R2 = T) |>
+  bayes_tbl_add_sig() |>
+  mutate_bayes_mod_beh() |>
+  rename_with(~paste0("cont_bv_", .x), 3:6) |>
+  left_join(
+    bayes_tbl_sum(mod.cont$ae, apa_table = T, add_loo_R2 = T) |>
+      bayes_tbl_add_sig() |>
+      mutate_bayes_mod_beh() |>
+      rename_with(~paste0("cont_ae_", .x), 3:6)
+  ) |>
+  # split
+  left_join(
+    bayes_tbl_sum(mod.dich$bv, apa_table = T, add_loo_R2 = T) |>
+      bayes_tbl_add_sig() |>
+      mutate_bayes_mod_beh() |>
+      rename_with(~paste0("exc_bv_", .x), 3:6)
+  ) |>
+  left_join(
+    bayes_tbl_sum(mod.dich$ae, apa_table = T, add_loo_R2 = T) |>
+      bayes_tbl_add_sig() |>
+      mutate_bayes_mod_beh() |>
+      rename_with(~paste0("exc_ae_", .x), 3:6)
+  )  |>
+  mutate(
+    c_ae_p   = if_else(as.numeric(cont_ae_p) >= .95, TRUE, FALSE),
+    c_bv_p   = if_else(as.numeric(cont_bv_p) >= .95, TRUE, FALSE),
+    e_ae_p   = if_else(as.numeric(exc_ae_p) >= .95, TRUE, FALSE),
+    e_bv_p   = if_else(as.numeric(exc_bv_p) >= .95, TRUE, FALSE),
+    diff_bv  = if_else(c_bv_p != e_bv_p, TRUE, FALSE),
+    diff_ae  = if_else(c_ae_p != e_ae_p, TRUE, FALSE),
+    e = ""
+  ) |> 
+  gt(groupname_col = "group") |>
+  tab_spanner("Behavioural variability", matches("_bv_")) |>
+  tab_spanner("Approximate Entropy", matches("_ae_")) |>
+  cols_hide(c(starts_with("exc"), starts_with("c_"), starts_with("e_"))) |>
+  cols_move(e, exc_bv_p) |>
+  tab_style(
+    cell_text(weight="bold"),
+    cells_body(matches("cont_bv"),diff_bv)
+  ) |>
+  tab_style(
+    cell_text(weight="bold"),
+    cells_body(matches("cont_ae"),diff_ae)
+  ) |>
+  cols_hide(starts_with("diff")) |>
+  tab_bayes_generics(pre_footnote = "Bold rows indicate a difference to the dichotomous Bayesian model.", 
+                     post_footnote = "Pre-positive = Pre-test positive mood, Pre-negative = Pre-test negative mood, PSD = partial sleep deprivation.") 
+
+conditional_save(
+  tbls[["cont_behav_models"]]
+  , "Dichotomous behaviour models"
+)
