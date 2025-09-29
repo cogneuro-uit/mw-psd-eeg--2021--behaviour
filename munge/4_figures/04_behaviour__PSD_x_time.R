706ee7375d268c526f72d41e1f30216eb78db843
Steffen Aasen <github.plop432@slmail.me>
2025-09-29 16:08:47 +0200

    Update fig: behaviour over time x PSD

--- munge/4_figures/04_behaviour__PSD_x_time.R
+++ munge/4_figures/04_behaviour__PSD_x_time.R
@@ -1,3 +1,4 @@
+# Approximate Entropy     ======
 # Data:
 figs[["behaviour__PSD_x_time--data"]] <-
   data.probe.mood.sleep |>
@@ -75,3 +76,30 @@ conditional_save(
   , "AE - Changes Over Time Across Sleep Loss"
   , width = 3, height = 3
 )
+
+
+
+# Behavioural variability     ======
+figs[["BV__PSD_x_time"]] <- 
+  plot_data |>
+  filter( probes == "Behavioural variability" ) |>
+  ggplot(aes(z_score, value, col = cond, linetype = cond)) + 
+  geom_hline(yintercept = 0, linetype = "dashed", alpha = .25) +
+  geom_line(linewidth = 1) +
+  labs(
+    y = "Z-Scored BV"
+    , x = "Probe number (Time-on-task)"
+    , col = "Condition"
+    , linetype = "Condition") +
+  scale_x_continuous(breaks = seq(0,1,1/(25/5)), labels = c(1, seq(5,25,5))) + 
+  scale_color_manual(   values = name_colour_interactions ) +
+  scale_linetype_manual(values = name_line_interactions ) +
+  theme(legend.position = "top", legend.direction = "horizontal")
+
+conditional_save(
+  figs[["BV__PSD_x_time"]]
+  , "BV - Changes Over Time Across Sleep Loss"
+)
+
+
+
