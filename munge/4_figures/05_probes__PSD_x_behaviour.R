3215793de63e8d5ec14ed76f7deab87957aaef54
Steffen Aasen <github.plop432@slmail.me>
2025-10-02 11:27:25 +0200

    Update fig probes~psd*behav code

--- munge/4_figures/05_probes__PSD_x_behaviour.R
+++ munge/4_figures/05_probes__PSD_x_behaviour.R
@@ -9,46 +9,47 @@ plot_data <-
     sleep = summarised_vals$sleep_m + (summarised_vals$sleep_sd * sleep_deviation),
     
     # MW ~ bv * psd 
-    , bv_ns = (mean(c$mw[["b_Intercept[1]"]]) + mean(c$mw[["b_Intercept[2]"]]) + mean(c$mw[["b_Intercept[3]"]]))
+    , mw_bv_ns = (mean(c$mw[["b_Intercept[1]"]]) + mean(c$mw[["b_Intercept[2]"]]) + mean(c$mw[["b_Intercept[3]"]]))
     + mean(c$mw[["b_zlogbv"]]) * z_score
-    , bv_psd = bv_ns
+    , mw_bv_psd = mw_bv_ns
     + mean(c$mw[["b_c.Adjusted_Duration.diff.pos"]]) * sleep 
     + mean(c$mw[["b_c.Adjusted_Duration.diff.pos:zlogbv"]]) * z_score * sleep
     
     # mw ~ AE * PSD
-    , ae_ns = (mean(c$mw[["b_Intercept[1]"]]) + mean(c$mw[["b_Intercept[2]"]]) + mean(c$mw[["b_Intercept[3]"]]))
+    , mw_ae_ns = (mean(c$mw[["b_Intercept[1]"]]) + mean(c$mw[["b_Intercept[2]"]]) + mean(c$mw[["b_Intercept[3]"]]))
     + mean(c$mw[["b_zlogapen"]]) * z_score
-    , ae_psd = ae_ns
+    , mw_ae_psd = mw_ae_ns
     + mean(c$mw[["b_c.Adjusted_Duration.diff.pos"]]) * sleep 
     + mean(c$mw[["b_c.Adjusted_Duration.diff.pos:zlogapen"]]) * z_score * sleep
     
     # MB ~ bv * psd
-    , mb_ns = (mean(c$mb[["b_Intercept[1]"]]) + mean(c$mb[["b_Intercept[2]"]]) + mean(c$mb[["b_Intercept[3]"]]))
+    , mb_bv_ns = (mean(c$mb[["b_Intercept[1]"]]) + mean(c$mb[["b_Intercept[2]"]]) + mean(c$mb[["b_Intercept[3]"]]))
     + mean(c$mb[["b_zlogbv"]]) * z_score
-    , mb_psd = mb_ns
+    , mb_bv_psd = mb_bv_ns
     + mean(c$mb[["b_c.Adjusted_Duration.diff.pos"]]) * sleep 
     + mean(c$mb[["b_c.Adjusted_Duration.diff.pos:zlogbv"]]) * z_score * sleep
     
   ) |>
   pivot_longer(c(ends_with("psd"), ends_with("ns")), names_to="names") |>
-  separate_wider_delim(names, "_", names_sep = "_", names = c("probe", "cond")) |>
+  separate_wider_delim(names, "_", names_sep = "_", names = c("probe", "behaviour", "cond")) |>
   mutate(cond = case_when(
     names_cond=="ns" ~ "NS"
     , sleep_deviation==-1 ~ "PSD -1 SD"
     , sleep_deviation==0 ~ "PSD Mean"
-    , sleep_deviation==+1 ~ "PSD +1 SD"
-  ) |> factor(levels = c("PSD +1 SD", "PSD Mean", "PSD -1 SD", "NS"))
+    , sleep_deviation==+1 ~ "PSD +1 SD") |> 
+      factor(levels = c("PSD +1 SD", "PSD Mean", "PSD -1 SD", "NS"))
   , probes = case_when(
-    names_probe=="bv" ~ "Behavioural variability"
-    , names_probe=="ae" ~ "Approximate entropy"
-    , names_probe=="mb" ~ "Mind blanking"
-    ) |> fct_relevel("Behavioural variability")
+    names_probe=="mb" ~ "Mind blanking"
+    , names_probe=="mw" ~ "Mind wandering") |> fct_relevel("Mind wandering")
+  , behaviour = case_when(
+    names_behaviour=="bv" ~ "Behavioural variability"
+    , names_behaviour=="ae" ~ "Approximate entropy") |> fct_relevel("Behavioural variability")
   ) 
 
 # BV    ======
 figs[["MW~PSD*BV"]] <- 
   plot_data |>
-  filter(probes=="Behavioural variability") |>
+  filter(behaviour=="Behavioural variability", probes == "Mind wandering") |>
   ggplot(aes(z_score, value, col = cond, linetype = cond)) +
   geom_line(linewidth = 1) +
   labs(
@@ -70,7 +71,7 @@ conditional_save(
 # AE        =======
 figs[["MW~PSD*AE"]] <-
   plot_data |>
-  filter(probes=="Approximate entropy") |>
+  filter(behaviour=="Approximate entropy", probes == "Mind wandering") |>
   ggplot(aes(z_score, value, col = cond, linetype = cond)) +
   geom_line(linewidth = 1) +
   labs(y = "Association with mind wandering", x = "Z-score AE", 
@@ -88,29 +89,33 @@ conditional_save(
 
 
 # COMBINE       =====
-figs[["MW~PSD*behaviour"]] <- 
+figs[["MW~PSD*behaviour--legend"]] <- 
   patchwork::guide_area() +
   figs[["MW~PSD*BV"]]  + figs[["MW~PSD*AE"]] +
   patchwork::plot_layout(ncol = 1, guides = "collect", heights = c(0.10, 1)
   , design = "11
   23")
   
-
 conditional_save(
-  figs[["MW~PSD*behaviour"]]
+  figs[["MW~PSD*behaviour--legend"]]
   , "Interaction - Behaviour on MW"
   , width = 5, height = 4
 )
 
+
+figs[["MW~PSD*behaviour"]] <- 
+  figs[["MW~PSD*BV"]] + theme(legend.position = "none")  + figs[["MW~PSD*AE"]]
+
+
 ## MW+MB ~ BV       ======
 figs[["(MW+MB)~PSD*BV"]] <-
   plot_data |>
-  filter(!(probes == "Approximate entropy")) |>
+  filter(behaviour == "Behavioural variability") |>
   ggplot(aes(z_score, value, col = cond, linetype = cond)) +
   facet_wrap(~probes) +
   coord_cartesian(ylim = c(1, 4)) + 
   geom_line(linewidth = 1) +
-  labs(y = "Association with mind blanking", x = "Z-score BV", 
+  labs(y = "Association with probe response", x = "Z-score BV", 
        col = "Condition", linetype="Condition") +
   scale_color_manual(   values = name_colour_interactions ) +
   scale_linetype_manual(values = name_line_interactions ) +
